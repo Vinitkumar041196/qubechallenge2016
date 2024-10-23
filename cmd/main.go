8c6a940e625e6421b552343041801d391a794095
Vinitkumar Gaikwad <vinit.kumar@currencysolutions.com>
2024-10-23 17:30:52 +0100

    http server init

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"distributor-manager/internal/app"
+	"distributor-manager/internal/server"
 	"distributor-manager/internal/store/localstore"
-	"distributor-manager/internal/types"
 	"log"
 )
 
@@ -19,34 +19,9 @@ func main() {
 		log.Fatal("could not initialize app error:", err)
 	}
 
-	err = distApp.PutDistributor(&types.Distributor{
-		Code: "1",
-		Permissions: &types.DistributorPermissions{
-			Include: []string{"IN", "US"},
-			Exclude: []string{"UP-IN", "YEOLA-MH-IN"},
-		},
-		SubDistributors: []*types.Distributor{
-			{
-				Code: "1-a",
-				Permissions: &types.DistributorPermissions{
-					Include: []string{"MH-IN"},
-					Exclude: []string{"US"},
-				},
-			},
-		},
-	})
+	server := server.NewServer(distApp)
+	err = server.Start()
 	if err != nil {
-		log.Fatal(" error:", err)
+		log.Fatal("failed to start server error:", err)
 	}
-
-	dist, err := distApp.GetDistributor("1")
-	if err != nil {
-		log.Fatal(" error:", err)
-	}
-	subDist, err := distApp.GetDistributor("1-a")
-	if err != nil {
-		log.Fatal(" error:", err)
-	}
-	log.Println("Dist", dist, dist.Permissions.Include, dist.Permissions.Exclude)
-	log.Println("subDist", subDist, subDist.Permissions.ToDistributorPermissionsMap())
 }
---
+++ internal/server/server.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"distributor-manager/internal/app"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	app    *app.App
+	router http.Handler
+}
+
+func NewServer(app *app.App) *Server {
+	srv := &Server{app: app}
+	srv.SetRouter()
+	return srv
+}
+
+func (srv *Server) SetRouter() {
+	router := gin.Default()
+	router.Use(cors.Default())
+	router.GET("/", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"message": "success"}) })
+
+	srv.router = router
+}
+
+func (srv *Server) Start() error {
+	address := os.Getenv("HTTP_ADDR")
+	if address == "" {
+		address = ":8080"
+	}
+
+	httpSrv := &http.Server{
+		Addr:         address,
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      srv.router,
+	}
+
+	return httpSrv.ListenAndServe()
+}
